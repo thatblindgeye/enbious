7294866b0ea0c2592a238ed08f6c234112326c9d
thatblindgeye <thatblindgeye@gmail.com>
2021-07-17 16:04:56 -0400

    Create individual component for cart items

--- src/components/Cards/CartCard.js
+++ src/components/Cards/CartCard.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import QuantitySelect from '../Inputs/QuantitySelect';
+import React, { useContext, useEffect } from 'react';
+import { CartDataContext } from '../../context/CartDataContext';
+import Quantity from '../Inputs/Quantity';
 
-export default function CartItem({
-    name,
-    quantity,
-    stock,
-    itemCost,
-    changeEvent,
-    removeEvent,
-}) {
+export default function CartItem({ cartItem, itemCost }) {
+    const [, dispatch] = useContext(CartDataContext);
+    const { name, quantity, stock } = cartItem;
+
+    const handleUpdateItem = (e) => {
+        return dispatch({
+            type: 'UPDATE_QUANTITY',
+            payload: {
+                ...cartItem,
+                quantity: Number(e.target.value),
+            },
+        });
+    };
+
+    const handleRemoveItem = (index) => {
+        return dispatch({
+            type: 'REMOVE_FROM_CART',
+            payload: {
+                ...cartItem,
+            },
+        });
+    };
+
     return (
         <div>
             <div>{name}</div>
             <div>{quantity}</div>
-            <QuantitySelect
-                changeEvent={changeEvent}
+            <Quantity
+                changeEvent={handleUpdateItem}
                 inputId='quantity'
                 stock={stock}
                 quantity={quantity}
             />
             <div>${itemCost}</div>
-            <button onClick={removeEvent}>Delete</button>
+            <button onClick={handleRemoveItem}>Delete</button>
         </div>
     );
 }
--- src/components/Cards/ItemCard.js
+++ src/components/Cards/ItemCard.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const StockWarning = ({ children }) => {
+    return <div id='stock-warning'>{children}</div>;
+};
+
 export default function ItemCard({ item }) {
+    const { id, name, stock } = item;
     return (
         <div>
             <h2>
-                <Link to={`/shop/${item.id}`} className='link'>
-                    {item.name}
+                <Link
+                    to={`/shop/${id}`}
+                    className='link'
+                    aria-describedby={stock < 10 ? 'stock-warning' : null}
+                >
+                    {name}
                 </Link>
             </h2>
+            {!stock ? (
+                <StockWarning>Sold Out</StockWarning>
+            ) : stock < 10 ? (
+                <StockWarning>Only {stock} left!</StockWarning>
+            ) : null}
         </div>
     );
 }
