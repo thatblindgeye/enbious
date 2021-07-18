0c13b82d22c450488e800366423ee99446a19dca
thatblindgeye <thatblindgeye@gmail.com>
2021-07-18 15:03:43 -0400

    Update layouts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,27 @@ import githubDark from '../../assets/images/logos/GitHub-White-Mark-32px.png';
 import githubLight from '../../assets/images/logos/GitHub-Black-Mark-32px.png';
 
 export default function Footer({ siteTheme }) {
-  const githubLogo = siteTheme === 'light' ? githubLight : githubDark;
+    const githubLogo = siteTheme === 'light' ? githubLight : githubDark;
 
-  return (
-    <footer>
-      <a href='https://github.com/thatblindgeye' className='link'>
-        <img src={githubLogo} alt='' className='github-link' />
-        @thatblindgeye on GitHub
-      </a>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className='upper-footer-nav'>
+                <a href='#' className='link'>
+                    Back to Top
+                </a>
+            </div>
+            <div className='lower-footer-nav'>
+                <a
+                    href='https://github.com/thatblindgeye'
+                    className='github-link'
+                >
+                    <img
+                        src={githubLogo}
+                        alt='thatblindgeye on GitHub'
+                        className='github-link-image'
+                    />
+                </a>
+            </div>
+        </footer>
+    );
 }
